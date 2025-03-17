7b5be3d239fdea14316ff3ba94e798d43c7b4461
StuartStephens <contact@stuartstephens.com>
2025-03-17 17:49:39 -0400

    show error messages, add recommended config

--- init.lua
+++ init.lua
@@ -742,8 +742,10 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
+    ---@module "conform"
+    ---@type conform.setupOpts
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
