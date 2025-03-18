217fb6103bdc6f80cd36aebc97e889bc7d7db201
StuartStephens <contact@stuartstephens.com>
2025-03-17 21:13:39 -0400

    fix description

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.keymap.set('n', '<leader>G', '<cmd>vertical G<cr>', { desc = 'Markdown Preview' })
+    vim.keymap.set('n', '<leader>G', '<cmd>vertical G<cr>', { desc = '[G]it Fugitive' })
   end,
 }
