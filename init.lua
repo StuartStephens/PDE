e5525872fe1e55651766d9de51daafae2b38e7ed
Stuart Stephens <stuart_stephens@homedepot.com>
2026-01-31 15:28:18 -0500

    update colorscheme to dracula

--- init.lua
+++ init.lua
@@ -902,11 +902,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'craftzdog/solarized-osaka.nvim',
+    'Mofiqul/dracula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('solarized-osaka').setup {
+      require('dracula').setup {
         -- require('tokyonight').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
@@ -919,7 +919,8 @@ require('lazy').setup({
       -- vim.cmd.colorscheme 'tokyonight-night'
       -- vim.cmd.colorscheme 'solarized-osaka'
       vim.opt.termguicolors = true
-      vim.cmd.colorscheme 'murphy'
+      -- vim.cmd.colorscheme 'murphy'
+      vim.cmd.colorscheme 'dracula'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
