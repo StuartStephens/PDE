374519225244202ead948a17c3a62d185af81f27
Stuart <contact@stuartstephens.com>
2026-05-31 12:00:54 -0400

    remove noice plugin config

--- lua/custom/plugins/noice.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add {
-  'https://github.com/folke/noice.nvim',
-  'https://github.com/MunifTanjim/nui.nvim',
-  'https://github.com/rcarriga/nvim-notify',
-}
-
-require('noice').setup {
-  -- add any options here
-}
